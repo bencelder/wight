041d352f3fd2a8f3ff58a1e7d97a2191abb09866
djbrout <djbrout@gmail.com>
2013-09-06 22:15:45 -0400

    preferences

--- pyPOD_GUI_tkinter.py
+++ pyPOD_GUI_tkinter.py
@@ -5,13 +5,21 @@ top = tk.Tk()
 def visitURL(url):
    webbrowser.open(url)
 
+def preferences():
+    pref = tk.Tk()
+    for i in range(0,10):
+        var = bool
+        l = tk.Checkbutton(pref, text=i,variable=var)
+        l.pack()
+    l.mainloop()
+
 # create a menu
 menu = tk.Menu(top)
 top.config(menu=menu)
 
 filemenu = tk.Menu(menu)
 menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="Preferences", command=None)
+filemenu.add_command(label="Preferences", command=preferences)
 
 
 link = "http://apod.nasa.gov/apod/image/1309/sgra_gasChandra.jpg"
@@ -19,3 +27,5 @@ linkname = "NASA Photo Of The Day"
 B = tk.Button(top, text =linkname, command = lambda:visitURL(link))
 B.pack()
 top.mainloop()
+
+
