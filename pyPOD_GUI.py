00e5bb61f0116783f696a732054f1bcd14572960
djbrout <djbrout@gmail.com>
2013-09-06 21:50:01 -0400

    tkinter file

--- pyPOD_GUI.py
+++ pyPOD_GUI.py
@@ -2,13 +2,12 @@
 
 # example treeviewcolumn.py
 
-#import pygtk as gtk
-#pygtk.require('2.0')
+import pygtk
+pygtk.require('2.0')
 import gtk
 import webbrowser
 
 class LinkTitle:
-    
     def delete_event(self, widget, event, data=None):
         gtk.main_quit()
         return False
@@ -25,6 +24,7 @@ class LinkTitle:
         link = "http://apod.nasa.gov/apod/image/1309/sgra_gasChandra.jpg"
         linkname = "NASA Photo Of The Day"
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title("pyPOD 1.0")
         self.window.connect("delete_event", self.delete_event)
         self.window.connect("destroy", self.destroy)
         self.window.set_border_width(10)
@@ -32,6 +32,12 @@ class LinkTitle:
         self.button.connect_object("clicked", self.visit_website, link)
         self.window.add(self.button)
         self.button.show()
+        toolbar = gtk.Toolbar()
+        toolbar.set_orientation(gtk.ORIENTATION_HORIZONTAL)
+        toolbar.set_style(gtk.TOOLBAR_BOTH)
+        toolbar.append_item("Preferences", "Opens preferences window", "", None, None, user_data=None)
+        self.window.add(toolbar)
+        toolbar.show()
         self.window.show()
         
 
