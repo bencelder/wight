17b66aca223ee3831856a5ab4e195e2ad3cf98d5
djbrout <djbrout@gmail.com>
2013-09-07 09:58:50 -0400

    haschanged.txt
    need to figure out how to notify the timer that the options have changed

--- pyPOD_GUI_tkinter.py
+++ pyPOD_GUI_tkinter.py
@@ -1,9 +1,12 @@
 import Tkinter as tk
 import webbrowser
+import pyPOD_timer
 top = tk.Tk()
 
-def visitURL(url):
-   webbrowser.open(url)
+def visitURL():
+    ci = open("currentimage.txt")
+    url = ci.readlines()[0]
+    webbrowser.open(url)
 
 def preferences():
     global pref
@@ -47,6 +50,9 @@ def prnt():
     if VOLvar.get():
         f.write(VOLlink+"\n")
     f.close()
+    f = open("haschanged.txt","w")
+    f.write("yes")
+    f.close()
     
     
 # create a menu
@@ -58,37 +64,15 @@ top.geometry("600x600")
 
 
 filemenu = tk.Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
+menu.add_cascade(label="Websites", menu=filemenu)
+
+#filemenu.add_command(label="Preferences", command=preferences)
 
 
-filemenu.add_command(label="Preferences", command=preferences)
-
-
-link = "http://apod.nasa.gov/apod/image/1309/sgra_gasChandra.jpg"
-linkname = "Current Image"
-B = tk.Button(top, text =linkname, command = lambda:visitURL(link))
+#link = "http://apod.nasa.gov/apod/image/1309/sgra_gasChandra.jpg"
+linkname = "Current Image URL"
+B = tk.Button(top, text =linkname, command = visitURL)
 B.pack()
-
-global NASAvar
-NASAvar = tk.BooleanVar()
-NASAbox = tk.Checkbutton(top, text="NASA Photo of the Day",variable=NASAvar,justify="left",command=prnt)
-NASAbox.pack()
-global NASAlink
-NASAlink = "http://apod.nasa.gov/apod/astropix.html"
-
-global NATGEOvar
-NATGEOvar = tk.BooleanVar()
-NATGEObox = tk.Checkbutton(top, text="NAT GEO Photo of the Day",variable=NATGEOvar,justify="left",command=prnt)
-NATGEObox.pack()
-global NATGEOlink
-NATGEOlink = "http://photography.nationalgeographic.com/photography/photo-of-the-day/"
-
-global VOLvar
-VOLvar = tk.BooleanVar()
-VOLbox = tk.Checkbutton(top, text="Volcano Discovery Photo of the Day",variable=VOLvar,justify="left",command=prnt)
-VOLbox.pack()
-global VOLlink
-VOLlink = "http://www.volcanodiscovery.com/photo-of-the-day.html"
 
 global period
 period = tk.StringVar(top)
@@ -96,6 +80,31 @@ period.set("Period") # default value
 
 w = tk.OptionMenu(top, period, "Period","1min","5min", "30min", "1hr","3hr","12hr","24hr")
 w.pack()
+
+
+global NASAvar
+NASAvar = tk.BooleanVar()
+#NASAbox = tk.Checkbutton(top, text="NASA Photo of the Day",variable=NASAvar,justify=tk.LEFT,command=prnt)
+NASAbox = filemenu.add_checkbutton(label="NASA Photo of the Day",  variable=NASAvar, command = prnt)
+#NASAbox.pack()
+global NASAlink
+NASAlink = "http://apod.nasa.gov/apod/astropix.html"
+
+global NATGEOvar
+NATGEOvar = tk.BooleanVar()
+#NATGEObox = tk.Checkbutton(top, text="NAT GEO Photo of the Day",variable=NATGEOvar,justify=tk.LEFT,command=prnt)
+NATGEObox = filemenu.add_checkbutton(label="NAT GEO Photo of the Day",  variable=NATGEOvar, command = prnt)
+#NATGEObox.pack()
+global NATGEOlink
+NATGEOlink = "http://photography.nationalgeographic.com/photography/photo-of-the-day/"
+
+global VOLvar
+VOLvar = tk.BooleanVar()
+#VOLbox = tk.Checkbutton(top, text="Volcano Discovery Photo of the Day",variable=VOLvar,justify=tk.LEFT,command=prnt)
+VOLbox = filemenu.add_checkbutton(label="Volcano Discover Photo of the Day",  variable=VOLvar, command = prnt)
+#VOLbox.pack()
+global VOLlink
+VOLlink = "http://www.volcanodiscovery.com/photo-of-the-day.html"
 
 top.mainloop()
 
--- pyPOD_timer.py
+++ pyPOD_timer.py
@@ -1,34 +1,40 @@
-import time
-import sys, time, msvcrt
-import shutil
+def run_timer():
+   import time
+   import sys, time, msvcrt
+   import shutil
 
-f = open('preferences.txt')
-lines = f.readlines()
-period = lines[0]
-f.close()
-if period == "1min":
-   sleeptime = 60
-if period == "5min":
-   sleeptime = 5*60
-if period == "30min":
-   sleeptime = 30*60
-if period == "1hr":
-   sleeptime = 60*60
-if period == "3hr":
-   sleeptime = 3*60*60
-if period == "12hr":
-   sleeptime = 12*60*60
-if period == "24hr":
-   sleeptime = 24*60*60
+   f = open('preferences.txt')
+   lines = f.readlines()
+   period = lines[0]
+   f.close()
+   f = open("haschanged.txt")
+   hs = f.readlines()
+   if period == "1min":
+      sleeptime = 60
+   if period == "5min":
+      sleeptime = 5*60
+   if period == "30min":
+      sleeptime = 30*60
+   if period == "1hr":
+      sleeptime = 60*60
+   if period == "3hr":
+      sleeptime = 3*60*60
+   if period == "12hr":
+      sleeptime = 12*60*60
+   if period == "24hr":
+      sleeptime = 24*60*60
 
-terminate = 0
-counter = -1
-numlinks = len(lines)- 1
-sleeptime = 3
-print sleeptime
-while terminate == 0:
-   counter = counter + 1
-   current_index = counter%numlinks + 1
-   time.sleep(sleeptime)
-   print lines[current_index]
+   terminate = 0
+   counter = -1
+   numlinks = len(lines)- 1
+   sleeptime = 3
+   print sleeptime
+   while terminate == 0:
+      counter = counter + 1
+      current_index = counter%numlinks + 1
+      time.sleep(sleeptime)
+      print lines[current_index]
+      ci = open("currentimage.txt","w")
+      ci.write(lines[current_index])
+      ci.close()
 
