1b08c2783b9941f4988b75f4b72111c78ce6a3be
djbrout <djbrout@gmail.com>
2013-09-06 23:04:59 -0400

    preferences file and save feature added

--- pyPOD_GUI_tkinter.py
+++ pyPOD_GUI_tkinter.py
@@ -7,18 +7,37 @@ def visitURL(url):
 
 def preferences():
     pref = tk.Tk()
-    for i in range(0,10):
-        var = bool
-        l = tk.Checkbutton(pref, text=i,variable=var)
-        l.pack()
-    l.mainloop()
+    pref.geometry("300x300")
+    counter = 0
+
+    NASAvar = tk.IntVar()
+    NASAbox = tk.Checkbutton(pref, text="NASA Photo of the Day",variable=NASAvar)
+    NASAbox.pack()
+
+
+    B = tk.Button(pref, text ="Save", command = lambda:save(pref,NASAvar))
+    B.pack()
+    pref.mainloop()
+
+def save(pref,NASAvar):
+    f = open('preferences.txt', 'w')
+    f.write(str(NASAvar))
+    pref.destroy()
+    
 
 # create a menu
 menu = tk.Menu(top)
 top.config(menu=menu)
 
+top.title("pyPOD 1.0")
+top.geometry("600x600")
+
+
 filemenu = tk.Menu(menu)
 menu.add_cascade(label="File", menu=filemenu)
+
+
+
 filemenu.add_command(label="Preferences", command=preferences)
 
 
@@ -27,5 +46,4 @@ linkname = "NASA Photo Of The Day"
 B = tk.Button(top, text =linkname, command = lambda:visitURL(link))
 B.pack()
 top.mainloop()
-
 
