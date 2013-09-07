9bca28354e8ad9b9cf63664f373b84b996fbda78
djbrout <djbrout@gmail.com>
2013-09-06 22:06:39 -0400

    button and toolbar added

--- pyPOD_GUI_tkinter.py
+++ pyPOD_GUI_tkinter.py
@@ -1,4 +1,21 @@
-import Tkinter
-top = Tkinter.Tk()
-# Code to add widgets will go here...
+import Tkinter as tk
+import webbrowser
+top = tk.Tk()
+
+def visitURL(url):
+   webbrowser.open(url)
+
+# create a menu
+menu = tk.Menu(top)
+top.config(menu=menu)
+
+filemenu = tk.Menu(menu)
+menu.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="Preferences", command=None)
+
+
+link = "http://apod.nasa.gov/apod/image/1309/sgra_gasChandra.jpg"
+linkname = "NASA Photo Of The Day"
+B = tk.Button(top, text =linkname, command = lambda:visitURL(link))
+B.pack()
 top.mainloop()
