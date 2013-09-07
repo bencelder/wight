eb4555a280c40b8b301812b02b516c4b00e10c32
djbrout <djbrout@gmail.com>
2013-09-07 00:28:17 -0400

    timer working (need bens function now)

--- pyPOD_timer.py
+++ pyPOD_timer.py
@@ -1,101 +1,34 @@
-import Tkinter as tk
-import webbrowser
-top = tk.Tk()
+import time
+import sys, time, msvcrt
+import shutil
 
-def visitURL(url):
-   webbrowser.open(url)
+f = open('preferences.txt')
+lines = f.readlines()
+period = lines[0]
+f.close()
+if period == "1min":
+   sleeptime = 60
+if period == "5min":
+   sleeptime = 5*60
+if period == "30min":
+   sleeptime = 30*60
+if period == "1hr":
+   sleeptime = 60*60
+if period == "3hr":
+   sleeptime = 3*60*60
+if period == "12hr":
+   sleeptime = 12*60*60
+if period == "24hr":
+   sleeptime = 24*60*60
 
-def preferences():
-    global pref
-    pref = tk.Tk()
-    pref.geometry("300x300")
-
-    global NASAvar
-    NASAvar = tk.BooleanVar()
-    NASAbox = tk.Checkbutton(pref, text="NASA Photo of the Day",variable=NASAvar)
-    NASAbox.pack()
-
-    global NATGEOvar
-    NATGEOvar = tk.BooleanVar()
-    NATGEObox = tk.Checkbutton(pref, text="NAT GEO Photo of the Day",variable=NATGEOvar)
-    NATGEObox.pack()
-
-    B = tk.Button(pref, text ="Save", command = save)
-    B.pack()
-    pref.mainloop()
-
-def save():
-    NASAvarg = NASAvar.get()
-    NATGEOvarg = NATGEOvar.get()
-    #print NASAvarg
-    #print NATGEOvarg
-    #print "variable is "+str(NASAvarg)
-    f = open('preferences.txt', 'w')
-    f.write(str(NASAvarg)+"\n")
-    f.write(str(NATGEOvarg)+"\n")
-    f.close()
-    #pref.destroy()
-    
-
-def prnt():
-    f = open('preferences.txt', 'w')
-    f.write(period.get()+"\n")
-    if NASAvar.get():
-        f.write(NASAlink+"\n")
-    if NATGEOvar.get():
-        f.write(NATGEOlink+"\n")
-    if VOLvar.get():
-        f.write(VOLlink+"\n")
-    f.close()
-    
-    
-# create a menu
-menu = tk.Menu(top)
-top.config(menu=menu)
-
-top.title("pyPOD 1.0")
-top.geometry("600x600")
-
-
-filemenu = tk.Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
-
-
-filemenu.add_command(label="Preferences", command=preferences)
-
-
-link = "http://apod.nasa.gov/apod/image/1309/sgra_gasChandra.jpg"
-linkname = "Current Image"
-B = tk.Button(top, text =linkname, command = lambda:visitURL(link))
-B.pack()
-
-global NASAvar
-NASAvar = tk.BooleanVar()
-NASAbox = tk.Checkbutton(top, text="NASA Photo of the Day",variable=NASAvar,justify="left",command=prnt)
-NASAbox.pack()
-global NASAlink
-NASAlink = "http://apod.nasa.gov/apod/astropix.html"
-
-global NATGEOvar
-NATGEOvar = tk.BooleanVar()
-NATGEObox = tk.Checkbutton(top, text="NAT GEO Photo of the Day",variable=NATGEOvar,justify="left",command=prnt)
-NATGEObox.pack()
-global NATGEOlink
-NATGEOlink = "http://photography.nationalgeographic.com/photography/photo-of-the-day/"
-
-global VOLvar
-VOLvar = tk.BooleanVar()
-VOLbox = tk.Checkbutton(top, text="Volcano Discovery Photo of the Day",variable=VOLvar,justify="left",command=prnt)
-VOLbox.pack()
-global VOLlink
-VOLlink = "http://www.volcanodiscovery.com/photo-of-the-day.html"
-
-global period
-period = tk.StringVar(top)
-period.set("Period") # default value
-
-w = tk.OptionMenu(top, period, "Period","1min","5min", "30min", "1hr","3hr","12hr","24hr")
-w.pack()
-
-top.mainloop()
+terminate = 0
+counter = -1
+numlinks = len(lines)- 1
+sleeptime = 3
+print sleeptime
+while terminate == 0:
+   counter = counter + 1
+   current_index = counter%numlinks + 1
+   time.sleep(sleeptime)
+   print lines[current_index]
 
