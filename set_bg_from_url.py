dfa113513e07874bb60f239ed85161a992651244
djbrout <djbrout@gmail.com>
2013-09-07 16:33:18 -0400

    try again

---
+++ set_bg_from_url.py
@@ -0,0 +1,11 @@
+import get_main_image as gmi
+
+# get the image
+url = 'http://photography.nationalgeographic.com/photography/photo-of-the-day/'
+
+gmi.get_main_image(url)
+# the image is now current_image
+
+# set it as the bg
+
+# change the fit pref
